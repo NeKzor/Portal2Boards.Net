1d6cd89dfaa9aa0b7dea630a21f99131538f168b
NeKz <nekzor@users.noreply.github.com>
2018-08-25 17:33:28 +0200

    Append library user agent

--- src/Portal2Boards.Net/API/ApiClient.cs
+++ src/Portal2Boards.Net/API/ApiClient.cs
@@ -5,40 +5,40 @@ using Newtonsoft.Json;
 
 namespace Portal2Boards.API
 {
-	internal sealed class ApiClient : IDisposable
-	{
-		private readonly HttpClient _client;
+    internal sealed class ApiClient : IDisposable
+    {
+        private readonly HttpClient _client;
 
-		public ApiClient(string userAgent)
-		{
-			_client = new HttpClient();
-			_client.DefaultRequestHeaders.UserAgent.ParseAdd
-			(
-				"Portal2Boards.Net/2.2" +
-				((!string.IsNullOrEmpty(userAgent)) ? $" {userAgent}" : string.Empty)
-			);
-		}
+        public ApiClient(string userAgent)
+        {
+            _client = new HttpClient();
+            _client.DefaultRequestHeaders.UserAgent.ParseAdd
+            (
+                ((!string.IsNullOrEmpty(userAgent)) ? userAgent : string.Empty)
+                + " Portal2Boards.Net/2.2"
+            );
+        }
 
-		public async Task<T> GetJsonObjectAsync<T>(string url)
-		{
-			var get = new HttpRequestMessage(HttpMethod.Get, url);
-			var response = await _client.SendAsync(get, HttpCompletionOption.ResponseContentRead).ConfigureAwait(false);
-			response.EnsureSuccessStatusCode();
-			return JsonConvert.DeserializeObject<T>(await response.Content.ReadAsStringAsync().ConfigureAwait(false));
-		}
+        public async Task<T> GetJsonObjectAsync<T>(string url)
+        {
+            var get = new HttpRequestMessage(HttpMethod.Get, url);
+            var response = await _client.SendAsync(get, HttpCompletionOption.ResponseContentRead).ConfigureAwait(false);
+            response.EnsureSuccessStatusCode();
+            return JsonConvert.DeserializeObject<T>(await response.Content.ReadAsStringAsync().ConfigureAwait(false));
+        }
 
-		// Gets the content of a demo file
-		public async Task<byte[]> GetBytesAsync(string url)
-		{
-			var get = new HttpRequestMessage(HttpMethod.Get, url);
-			var response = await _client.SendAsync(get, HttpCompletionOption.ResponseContentRead).ConfigureAwait(false);
-			response.EnsureSuccessStatusCode();
-			return await response.Content.ReadAsByteArrayAsync().ConfigureAwait(false);
-		}
+        // Gets the content of a demo file
+        public async Task<byte[]> GetBytesAsync(string url)
+        {
+            var get = new HttpRequestMessage(HttpMethod.Get, url);
+            var response = await _client.SendAsync(get, HttpCompletionOption.ResponseContentRead).ConfigureAwait(false);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsByteArrayAsync().ConfigureAwait(false);
+        }
 
-		public void Dispose()
-		{
-			_client.Dispose();
-		}
-	}
+        public void Dispose()
+        {
+            _client.Dispose();
+        }
+    }
 }
